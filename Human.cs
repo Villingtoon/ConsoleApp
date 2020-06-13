7548b627a000ecd60444e7d689b6252b9f647b6d
Noob gamer <legofanslv@gmail.com>
2020-06-13 17:41:39 +0300

    Add project files.

---
+++ Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string EyeColor { get; set; }
+
+        public Human()
+        {
+
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi!");
+            Console.WriteLine("My full name is " + this.Name + " " + this.Surname + ".");
+            Console.WriteLine("I am " + this.Age + " years old.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person = new Human();
+            Console.WriteLine("Enter your name:");
+            person.Name = Console.ReadLine();
+            Console.WriteLine("Enter your surname:");
+            person.Surname = Console.ReadLine();
+            person.Age = 25;
+            person.EyeColor = "Grey";
+
+            person.Introduce();
+
+            //Console.WriteLine(person.Name + " " + person.Surname);
+
+            //person.Name = "Janis";
+
+            //Console.WriteLine(person.Name + " " + person.Surname);
+        }
+    }
+}
